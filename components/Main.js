e3d9d34e0bad70584aa718a0fb740ada6b8cdedb
NCJo <cnimkulr@gmail.com>
2019-05-05 23:33:04 -0600

    Added: name text box for user input

--- components/Main.js
+++ components/Main.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
 import {
     StyleSheet,
+    Text,
     TextInput,
+    TouchableOpacity,
     View,
 } from 'react-native';
 
 class Main extends Component {
     state = { name: '' } // use for tracking update and user input
+    onPress = () => {
+        this.props.navigation.navigate('Chat', { name: this.state.name });
+    }
     onChangeText = name => this.setState({ name }); // update the component with a new string value
 
     render() {
         return (
             <View>
+                {/* Title */}
+                <Text style={styles.title}>Enter your name:</Text>
+
+                {/* Input box */}
                 <TextInput
                     onChangeText={this.onChangeText}
                     style={styles.nameInput}
                     placeHolder="John Cena"
                     value={this.state.name}    
                 />
+
+                {/* Button */}
+                <TouchableOpacity onPress={this.onPress}>
+                    <Text style={styles.buttonText}>Next</Text>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -26,12 +39,21 @@ class Main extends Component {
 
 const offset = 24;
 const styles = StyleSheet.create({
+    title: {
+        marginTop: offset,
+        marginLeft: offset,
+        fontSize: offset,
+    },
     nameInput: {
         height: offset * 2,
         margin: offset,
         paddingHorizontal: offset,
         borderColor: '#111111',
         borderWidth: 1,
+    },
+    buttonText: {
+        marginLeft: offset,
+        fontSize: offset,
     },
 });
 
